9562ee0a8f47ae2ccab58f553def294bbac864bf
unknown <sc6472@nyu.edu>
2020-10-13 15:50:49 +0800

    finish hw4

---
+++ hw4/Mapper.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class Mapper
+  extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+  
+  @Override
+  public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException {
+    
+    String[] lines = value.toString().split("\n");
+    String[] info;
+    for(int i = 1 ; i < lines.length; i++){
+        info = lines[i].split(",");
+        context.write(new Text(info[4] + " "+info[5]), new IntWritable(1));
+    }
+    
+  }
+}
