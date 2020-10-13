0e53109938b4a5f8e3fb81e4f075a92bf81a72bf
unknown <sc6472@nyu.edu>
2020-10-13 17:28:00 +0800

    change scripts

--- hw4/AirbnbMapper.java
+++ hw4/AirbnbMapper.java
@@ -13,8 +13,8 @@ public class AirbnbMapper
   public void map(LongWritable key, Text value, Context context)
       throws IOException, InterruptedException {
     
-    String info = value.toString();
-    context.write(new Text(info.length() + " "), new IntWritable(1));
+    String info = value.toString().length() ;
+    context.write(new Text(info), new IntWritable(1));
     
     
   }
