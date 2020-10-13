9c5a297b5cf6f5344730c3b8b742eadabd6b7935
unknown <sc6472@nyu.edu>
2020-10-13 17:10:52 +0800

    change scripts

--- hw4/AirbnbMapper.java
+++ hw4/AirbnbMapper.java
@@ -15,7 +15,6 @@ public class AirbnbMapper
     
     String[] info = value.toString().split(",");
     for(int i = 1 ; i < info.length; i++){
-        info = lines[i].split(",");
         context.write(new Text(info[4] + " "+info[5]), new IntWritable(1));
     }
     
