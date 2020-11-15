e2e74b294d080796107325929d6dc6ab5b8ee688
unknown <sc6472@nyu.edu>
2020-11-16 00:01:26 +0800

    project

--- hw4/CountRecsReducer.java
+++ hw4/CountRecsReducer.java
@@ -8,6 +8,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 public class CountRecsReducer
   extends Reducer<Text, Text, NullWritable, IntWritable> {
   
+    @Override
   public void reduce(Text key, Iterable<IntWritable> values,
       Context context)
       throws IOException, InterruptedException {
@@ -15,6 +16,6 @@ public class CountRecsReducer
     for (IntWritable value : values) {
       count++;
     }
-    context.write(NullWritable.get(), new IntWritable(count));
+    context.write(NullWritable.get(), key);
   }
 }
