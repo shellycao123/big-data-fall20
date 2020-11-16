9c0306629605858d42eb37855285378ed4ace0f5
unknown <sc6472@nyu.edu>
2020-11-16 09:33:12 +0800

    project scrirpt

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 # Remove class and jar files
 rm *.class
 rm *.jar
-hdfs dfs -rm -r -f project_output/cleaned_count
+hdfs dfs -rm -r -f project_output/origin_count
 # Compile
 javac -classpath `yarn classpath` -d . ../hw4/CountRecsMapper.java
 javac -classpath `yarn classpath` -d . ../hw4/CountRecsReducer.java
@@ -13,6 +13,6 @@ javac -classpath `yarn classpath`:. -d . ../hw4/CountRecs.java
 jar -cvf CountRecs.jar *.class
 
 # Run the program
-hadoop jar CountRecs.jar CountRecs /user/sc6472/project_input/cleaned/cleaned.csv /user/sc6472/project_output/cleaned_count
-hdfs dfs -cat project_output/cleaned_count/part-r-00000
-hdfs dfs -copyToLocal project_output/cleaned_count/part-r-00000 /home/sc6472/cleaned_count.csv
+hadoop jar CountRecs.jar CountRecs /user/sc6472/project_input/origin/hbo.csv /user/sc6472/project_output/origin_count
+hdfs dfs -cat project_output/origin_count/part-r-00000
+hdfs dfs -copyToLocal project_output/origin_count/part-r-00000 /home/sc6472/origin_count.csv
