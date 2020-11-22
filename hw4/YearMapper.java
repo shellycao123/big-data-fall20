0de71065b0ac1aee54f5416ca4552db094196f4c
unknown <sc6472@nyu.edu>
2020-11-22 22:05:08 +0800

    script

--- hw4/YearMapper.java
+++ hw4/YearMapper.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.IntWritable;
+import java.util.Integer;
+import java.Exception;
 
 
 public class YearMapper
@@ -13,8 +15,15 @@ public class YearMapper
   @Override
   public void map(LongWritable key, Text value, Context context)
       throws IOException, InterruptedException {
-    String[] info = value.toString().split(",");
-    context.write(new Text(info[2]), new IntWritable(1));
+    try{
+        String[] info = value.toString().split(",");
+        int year = Integer.parseInt(info[2]);
+        context.write(new Text(info[2]), new IntWritable(1));
+    }
+    catch(Exception e){
+        context.write(new Text(info), new IntWritable(1));
+    }
+
     
   }
 
