5dada084ddd4d81c2d764e12aef6014763da0107
unknown <sc6472@nyu.edu>
2020-11-22 12:16:09 +0800

    year distribution

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,16 +3,16 @@
 # Remove class and jar files
 rm *.class
 rm *.jar
-hdfs dfs -rm -r -f project_output/origin_Year
+hdfs dfs -rm -r -f project_output/year_distribution
 # Compile
 javac -classpath `yarn classpath` -d . ../hw4/YearMapper.java
 javac -classpath `yarn classpath` -d . ../hw4/YearReducer.java
 javac -classpath `yarn classpath`:. -d . ../hw4/Year.java
 
 # Create jar file
-jar -cvf years.jar *.class
+jar -cvf year.jar *.class
 
 # Run the program
-hadoop jar years.jar Years /user/sc6472/project_input/origin/hbo.csv /user/sc6472/project_output/year_distribution
+hadoop jar year.jar Year /user/sc6472/project_input/origin/hbo.csv /user/sc6472/project_output/year_distribution
 hdfs dfs -cat project_output/year_distribution/part-r-00000
 hdfs dfs -copyToLocal project_output/year_distribution/part-r-00000 /home/sc6472/year_distribution.txt
