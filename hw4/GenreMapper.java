c97b11ee83f1a29f97701306dc4aa5ee6c92fe54
unknown <sc6472@nyu.edu>
2020-11-22 22:49:59 +0800

    script

--- hw4/GenreMapper.java
+++ hw4/GenreMapper.java
@@ -13,10 +13,9 @@ public class GenreMapper
   @Override
   public void map(LongWritable key, Text value, Context context)
       throws IOException, InterruptedException {
-    String[] info = value.toString().split(",\"");
+    String[] info = value.toString().split("[,\"]");
     for(int i = 3; i < info.length; i++){
         context.write(new Text(info[i]), new IntWritable(1));
-
     }
 
 
