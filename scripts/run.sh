58adbda6c1636fdf45a8edb9d6c78dfd6369ec3e
unknown <sc6472@nyu.edu>
2020-11-29 20:39:57 +0800

    script

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,16 +3,16 @@
 # Remove class and jar files
 rm *.class
 rm *.jar
-hdfs dfs -rm -r -f project/project_output/Year_distribution
+hdfs dfs -rm -r -f project/project_output/Genre_distribution
 # Compile
-javac -classpath `yarn classpath` -d . ../hw4/ana_code/YearMapper.java
-javac -classpath `yarn classpath` -d . ../hw4/ana_code/YearReducer.java
-javac -classpath `yarn classpath`:. -d . ../hw4/ana_code/Year.java
+javac -classpath `yarn classpath` -d . ../hw4/ana_code/GenreMapper.java 
+javac -classpath `yarn classpath` -d . ../hw4/ana_code/GenreReducer.java
+javac -classpath `yarn classpath`:. -d . ../hw4/ana_code/Genre.java
 
 # Create jar file
-jar -cvf Year.jar *.class
+jar -cvf Genre.jar *.class
 
 # Run the program
-hadoop jar Year.jar Year /user/sc6472/project/project_input/cleaned/cleaned.csv /user/sc6472/project/project_output/Year_distribution
-hdfs dfs -cat project/project_output/Year_distribution/part-r-00000
-hdfs dfs -copyToLocal project/project_output/Year_distribution/part-r-00000 /home/sc6472/big-data-fall20/hw4/Year_distribution.txt
+hadoop jar Genre.jar Genre /user/sc6472/project/project_input/cleaned/cleaned.csv /user/sc6472/project/project_output/Genre_distribution
+hdfs dfs -cat project/project_output/Genre_distribution/part-r-00000
+hdfs dfs -copyToLocal project/project_output/Genre_distribution/part-r-00000 /home/sc6472/big-data-fall20/hw4/Genre_distribution.txt
