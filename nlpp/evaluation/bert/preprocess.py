b3efa29199a2fd5042b4a293027acf6e8296f2eb
unknown <sc6472@nyu.edu>
2020-12-07 10:00:50 +0800

    preprocess bert

--- nlpp/evaluation/bert/preprocess.py
+++ nlpp/evaluation/bert/preprocess.py
@@ -1,5 +1,5 @@
 import sys
-
+import random
 f = open(sys.argv[1], 'r', encoding='utf-8')
 updated = open(sys.argv[1][:len(sys.argv[1]) - 4] + "_bert.csv", 'a', encoding='utf-8')
 answers = []
@@ -11,9 +11,9 @@ f.seek(0)
 
 for i, line in enumerate(f):
 	sent = line.split("\t")
-	for j in range(len((answers))):
-		if i != j:
-			
+	ind = random.choice(range(len(answers)), k=10)
+	for j in ind:
+		if i != j:			
 			updated.write(sent[0] + "\t" + answers[j] + "\t0\n")
 
 updated.close()
